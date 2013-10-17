8add9587555588337e09c73356396fa7fe5ff5cf
siggihj <siggmeister@gmail.com>
2013-10-17 14:51:27 +0000

    siggis commit

--- NIM.cpp
+++ NIM.cpp
@@ -11,7 +11,7 @@ void birta( int* hruga );
 
 //Şetta fall á ağ reikna út hversu margar eldspıtur eru eftir í hrúgunum:
 int samtals( int* hruga );
-
+cout <<"Sigurğur Högni Jónsson skrifadi thetta"<< endl; 
 //Şetta fall lætur tölvuna gera:
 void tolva( int* hruga );
 
